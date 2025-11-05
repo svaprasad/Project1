7d37bf91a025102eb4e8705e2cd508c9f7a49c65
svaprasad <sivaprasad.ch@oppty.in>
2025-11-05 18:47:36 +0530

    Day 22 javascript
    variables,arrays,string....etc

---
+++ Day 21 javascript/practice.js
@@ -0,0 +1,228 @@
+
+
+// document.getElementById("demo") .innerHTML = "hi java script";
+
+
+
+
+ let text1 = "Hi Oppty";
+        
+ 
+        document.getElementById("value").innerHTML = "text1";
+        
+        console.log(text1); 
+
+
+        
+// document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+// document.getElementsByClassName("name:siva prasad").innerHTML = "<h3> Helloo oppty </h2>";
+
+
+                // ----------addition-----
+
+// let x = 5;
+// let y = 6;
+
+// let z = x + y;  
+
+// console.log(z)
+
+
+// -----------practice2---------
+
+// let b = 10;
+// let c = 10;
+// what is a value ?
+
+// let a = b+c;
+
+// console.log(a);
+
+
+// --------practcie 3-------------
+
+// let d = 10;
+// let e = 20;
+
+// let f = a+b;
+
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+
+
+// -------name------
+
+
+// let name = "siva prasad";
+
+// console.log(name);
+
+
+// Strings
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// Number
+// let length = 16;
+// let weight = 7.5;
+
+
+
+// Boolean
+// let g = true;
+// let h = false;
+
+// Object
+// const person = {firstName:"John", lastName:"Doe"};
+
+// console.log(person);
+// Array object
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// console.log(cars);
+
+
+// Date object
+// const date = new Date("2022-03-25");
+
+// Undefined
+// let x;
+// let y;
+
+// Null
+// let x = null;
+// let y = null;
+
+
+
+// Data Types
+// let text = "Hello, World!"; // String
+// let num = 10; // Number
+// let isActive = true; // Boolean ture/ false
+// let x; // Undefined
+// let person = null; // Null
+// let colors = ["green", "skyblue", "red", "blue"]; // Array
+
+
+// console.log(colors[3]);
+
+
+
+
+
+// let text = "Hello, World!";
+// document.getElementById("value").innerHTML = text;
+// console.log(text); 
+
+
+
+               // ----------addition-----
+
+// let x = 5;
+// let y = 6;
+
+// let z = x + y;  
+
+// console.log(z)
+
+
+// -----------practice2---------
+
+// let b = 10;
+// let c = 10;
+// what is a value ?
+
+// let a = b+c;
+
+// console.log(a);
+
+
+// --------practcie 3-------------
+
+// let d = 10;
+// let e = 20;
+
+// let f = a+b;
+
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+
+
+// -------name------
+
+
+// let name = "siva prasad";
+
+// console.log(name);
+
+
+// Strings
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// Number
+// let length = 16;
+// let weight = 7.5;
+
+
+
+// Boolean
+// let g = true;
+// let h = false;
+
+// Object
+// const person = {firstName:"John", lastName:"Doe"};
+
+// console.log(person);
+// Array object
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// console.log(cars);
+
+
+// Date object
+// const date = new Date("2022-03-25");
+
+// Undefined
+// let x;
+// let y;
+
+// Null
+// let x = null;
+// let y = null;
+
+// document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+// document.getElementsByClassName("name:siva prasad").innerHTML = "<h3> Helloo oppty </h2>";
+
+
+// Data Types
+let text = "Hello, World!"; // String
+let num = 10; // Number
+let isActive = true; // Boolean ture/ false
+let x; // Undefined
+let person = null; // Null
+let colors = ["green", "skyblue", "red", "blue"]; // Array
+
+
+console.log(colors[3]);
+
+
+
+
+
+// let text = "Hello, World!";
+// document.getElementById("value").innerHTML = text;
+// console.log(text); 
+
+
+
+
+
