5c674e2aea59065a41cf3b4efcd766b756eb460c
svaprasad <sivaprasad.ch@oppty.in>
2025-11-17 19:11:48 +0530

    Day30 commit

--- Day30/Toggle/toggle.js
+++
@@ -1,6 +0,0 @@
- 
-    function toggleMode() {
-        // Toggle between light and dark classes
-        document.body.classList.toggle("dark");
-        document.body.classList.toggle("light");
-    }
